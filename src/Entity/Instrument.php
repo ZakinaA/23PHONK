c8a2508efce13904db7b73e30d3807b9dcdc7456
HRVNoa <hervieu.noa@gmail.com>
2023-11-15 11:14:51 +0100

    Création de l'entité Classe Instrument

---
+++ src/Entity/Instrument.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InstrumentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InstrumentRepository::class)]
+class Instrument
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $numSerie = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateAchat = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $prixAchat = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $utilisation = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $cheminImage = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumSerie(): ?string
+    {
+        return $this->numSerie;
+    }
+
+    public function setNumSerie(string $numSerie): static
+    {
+        $this->numSerie = $numSerie;
+
+        return $this;
+    }
+
+    public function getDateAchat(): ?\DateTimeInterface
+    {
+        return $this->dateAchat;
+    }
+
+    public function setDateAchat(\DateTimeInterface $dateAchat): static
+    {
+        $this->dateAchat = $dateAchat;
+
+        return $this;
+    }
+
+    public function getPrixAchat(): ?string
+    {
+        return $this->prixAchat;
+    }
+
+    public function setPrixAchat(string $prixAchat): static
+    {
+        $this->prixAchat = $prixAchat;
+
+        return $this;
+    }
+
+    public function getUtilisation(): ?string
+    {
+        return $this->utilisation;
+    }
+
+    public function setUtilisation(string $utilisation): static
+    {
+        $this->utilisation = $utilisation;
+
+        return $this;
+    }
+
+    public function getCheminImage(): ?string
+    {
+        return $this->cheminImage;
+    }
+
+    public function setCheminImage(?string $cheminImage): static
+    {
+        $this->cheminImage = $cheminImage;
+
+        return $this;
+    }
+}
