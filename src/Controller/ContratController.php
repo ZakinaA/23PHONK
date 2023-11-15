14a83b4a6a3ea8ecb6735e1a7093298c822cc440
JanekBigoin <Bigoinjanek@gmail.com>
2023-11-15 11:48:50 +0100

    Mise en place de la route pour lister les contrats

---
+++ src/Controller/ContratController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contrat;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContratController extends AbstractController
+{
+    //#[Route('/contrat', name: 'app_contrat')]
+    public function index(): Response
+    {
+        return $this->render('contrat/index.html.twig', [
+            'controller_name' => 'ContratController',
+        ]);
+    }
+
+    public function contratLister(ManagerRegistry $doctrine){
+
+        $repository = $doctrine->getRepository(Contrat::class);
+
+        $contrats= $repository->findAll();
+        return $this->render('contrat/lister.html.twig', [
+            'pContrats' => $contrats,]);
+
+    }
+}
