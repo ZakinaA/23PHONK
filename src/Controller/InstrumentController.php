c7f5603cde0d65e5f640f1348230df9a7ba96199
HRVNoa <hervieu.noa@gmail.com>
2023-11-20 15:23:18 +0100

    Ajout de la fonction consulter par id et récupération de la liste des interventions en fonction des id

--- src/Controller/InstrumentController.php
+++ src/Controller/InstrumentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Etudiant;
 use App\Entity\Instrument;
+use App\Entity\Intervention;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,5 +26,23 @@ class InstrumentController extends AbstractController
         $instrument= $repository->findAll();
         return $this->render('instrument/lister.html.twig', [
             'pInstruments' => $instrument,]);
+    }
+    public function consulter(ManagerRegistry $doctrine, int $id){
+
+        $instrument= $doctrine->getRepository(Instrument::class)->find($id);
+
+
+        $repository = $doctrine->getRepository(Intervention::class);
+        $intervention= $repository->findBy(['id' => $id]);
+
+        if (!$instrument) {
+            throw $this->createNotFoundException(
+                'Aucun instrument trouvé avec le numéro '.$id
+            );
+        }
+
+        //return new Response('Etudiant : '.$etudiant->getNom());
+        return $this->render('instrument/consulter.html.twig',
+            ['instrument' => $instrument, 'pIntervention' => $intervention]);
     }
 }
