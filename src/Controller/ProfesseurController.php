ac92d3a9b8b5a652c40e17c020a3ef5c82c51ac7
BouquetThomas <darkelixor@gmail.com>
2023-11-22 11:48:06 +0100

    Ajout d'une fonction pour ajouter un professeur

--- src/Controller/ProfesseurController.php
+++ src/Controller/ProfesseurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\ProfesseurType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -46,4 +47,24 @@ class ProfesseurController extends AbstractController
             'typeInstruments' => $typesInstruments,]);
     }
 
+    public function ajouter(ManagerRegistry $doctrine,Request $request){
+        $professeur = new professeur();
+        $form = $this->createForm(ProfesseurType::class, $professeur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $professeur = $form->getData();
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($professeur);
+            $entityManager->flush();
+
+            return $this->render('professeur/consulter.html.twig', ['professeu' => $professeur,]);
+        }
+        else
+        {
+            return $this->render('professeur/ajouter.html.twig', array('form' => $form->createView(),));
+        }
+    }
 }
