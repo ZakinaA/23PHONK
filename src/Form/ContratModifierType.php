5ea58253d1133417d83e2ec5f3dbd4b011804b0b
JanekBigoin <Bigoinjanek@gmail.com>
2023-11-22 10:45:31 +0100

    Formulaire pour modifier un contrat de pret

--- src/Form/ContratModifierType.php
+++ src/Form/ContratModifierType.php
@@ -3,27 +3,46 @@
 namespace App\Form;
 
 use App\Entity\Contrat;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContratModifierType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dateDebut')
-            ->add('dateFin')
+            ->add('dateDebut', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
+            ->add('dateFin', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
             ->add('attestationAssurance')
             ->add('etatDetailleDebut')
             ->add('etatDetailleFin')
-            ->add('eleve')
-            ->add('instrument')
-            ->add('interventions')
+            ->add('eleve', EntityType::class, array('class' => 'App\Entity\Eleve','choice_label' => 'nom'))
+            ->add('instrument', EntityType::class, array('class' => 'App\Entity\Instrument','choice_label' => 'numSENE'))
+            ->add('interventions', EntityType::class, [
+                'class' => 'App\Entity\Intervention',
+                'choice_label' => 'decriptif',
+                'multiple' => true,
+                'expanded' => true,])
+
+            ->add('enregistrer', SubmitType::class, array('label' => 'Modifier contrat'))
         ;
+        ;
+
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Contrat::class,
