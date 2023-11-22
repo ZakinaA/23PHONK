7a0f26f9fcd459271293ef94c4b2cba00fe33b93
JanekBigoin <Bigoinjanek@gmail.com>
2023-11-22 10:22:49 +0100

    Creéation de ContratModifierType

---
+++ src/Form/ContratModifierType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contrat;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContratModifierType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateDebut')
+            ->add('dateFin')
+            ->add('attestationAssurance')
+            ->add('etatDetailleDebut')
+            ->add('etatDetailleFin')
+            ->add('eleve')
+            ->add('instrument')
+            ->add('interventions')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contrat::class,
+        ]);
+    }
+}
