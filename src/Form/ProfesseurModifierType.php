40d368fe65de2a6e3ddbbf0494d6595523ad8cf4
BouquetThomas <darkelixor@gmail.com>
2023-11-22 11:55:55 +0100

    Création du formulaire pour modifier un professeur

---
+++ src/Form/ProfesseurModifierType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Professeur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class ProfesseurModifierType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('numRue')
+            ->add('rue')
+            ->add('copos')
+            ->add('ville')
+            ->add('tel')
+            ->add('mail')
+            ->add('typeInstruments',
+                EntityType::class, [
+                    'class' => 'App\Entity\TypeInstrument',
+                    'choice_label' => 'libelle',
+                    'multiple' => true,
+                    'expanded' => true,])
+            ->add('enregistrer', SubmitType::class, array('label' => 'Nouveau Professeur'))
+        ;
+    }
+
+    public function getParent(){
+        return ProfesseurType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Professeur::class,
+        ]);
+    }
+}
