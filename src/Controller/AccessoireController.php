1b73480a87c6000532586afba2c6b08f58079ff5
HRVNoa <hervieu.noa@gmail.com>
2023-11-29 09:31:13 +0100

    Ajout fonction lister

--- src/Controller/AccessoireController.php
+++ src/Controller/AccessoireController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Accessoire;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccessoireController extends AbstractController
 {
-    #[Route('/accessoire', name: 'app_accessoire')]
+//    #[Route('/accessoire', name: 'app_accessoire')]
     public function index(): Response
     {
         return $this->render('accessoire/index.html.twig', [
             'controller_name' => 'AccessoireController',
         ]);
+    }
+
+    public function lister(ManagerRegistry $doctrine){
+
+        $repository = $doctrine->getRepository(Accessoire::class);
+
+        $accessoire= $repository->findAll();
+        return $this->render('accessoire/lister.html.twig', [
+            'pAccessoires' => $accessoire,]);
     }
 }
