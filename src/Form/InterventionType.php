76559f89baa5282d0c96e0a26c7958bf54a6e207
JanekBigoin <Bigoinjanek@gmail.com>
2023-11-29 11:54:50 +0100

    Ajout Intervention type

---
+++ src/Form/InterventionType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Intervention;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InterventionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateDebut')
+            ->add('dateFin')
+            ->add('descriptif')
+            ->add('prix')
+            ->add('enregistrer', SubmitType::class, array('label' => 'Nouvelle intervention'))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Intervention::class,
+        ]);
+    }
+
+}
