5f430c215aabd356661206458e39a6d71b58db1a
BouquetThomas <darkelixor@gmail.com>
2023-11-29 09:48:06 +0100

    Supprimer libelle de cours

--- src/Entity/Cours.php
+++ src/Entity/Cours.php
@@ -16,9 +16,6 @@ class Cours
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $libelle = null;
-
     #[ORM\Column(nullable: true)]
     private ?int $ageMini = null;
 
@@ -57,18 +54,6 @@ class Cours
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getLibelle(): ?string
-    {
-        return $this->libelle;
-    }
-
-    public function setLibelle(string $libelle): static
-    {
-        $this->libelle = $libelle;
-
-        return $this;
     }
 
     public function getAgeMini(): ?int
