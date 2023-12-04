b8e2a5e2c505c60e8474c0e496c806175518ef6b
JanekBigoin <Bigoinjanek@gmail.com>
2023-12-04 13:21:50 +0100

    Ajout du templates sur les Interventions

--- src/Form/InterventionType.php
+++ src/Form/InterventionType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Intervention;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,11 +14,36 @@ class InterventionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('dateDebut')
-            ->add('dateFin')
-            ->add('descriptif')
-            ->add('prix')
-            ->add('enregistrer', SubmitType::class, array('label' => 'Nouvelle intervention'))
+            ->add('dateDebut', DateType::class, [
+                'label' => 'Date au début de l\'intervention',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'attr' => ['class' => 'form-control form-control-user'],
+
+            ])
+
+            ->add('dateFin', DateType::class, [
+                'label' => 'Date de la fin de l\'intervention',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'attr' => ['class' => 'form-control form-control-user'],
+
+            ])
+
+            ->add('descriptif', null, [
+                'label' => 'Descriptif de l\'intervention',
+                'attr' => ['class' => 'form-control form-control-user'],
+
+            ])
+
+            ->add('prix', null, [
+                'label' => 'Prix de l\'intervention',
+                'attr' => ['class' => 'form-control form-control-user'],
+
+            ])
+
+
+            ->add('enregistrer', SubmitType::class, array('label' => 'Nouvelle intervention', 'attr' => ['class' => 'btn btn-primary btn-user btn-block']))
         ;
     }
 
